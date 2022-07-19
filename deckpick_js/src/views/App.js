9919ea58c6d468f4a87c665006923ce8326a1088
Oscar Persson <opers@kth.se>
2022-07-19 02:05:31 +0200

    #8 - FEAT: Adds logo and changes background elements to img from NextUI's Image

--- deckpick_js/src/views/App.js
+++ deckpick_js/src/views/App.js
@@ -1,16 +1,25 @@
 // Library imports.
 import React from 'react';
-import { NextUIProvider } from '@nextui-org/react';
+import { NextUIProvider, Image, Text, Col, Row } from '@nextui-org/react';
 
 // Component imports.
 import { SwipePresenter } from '../components/swipePresenter';
 import image from '../assets/svg/wave_1.svg';
+import logo from '../assets/raster/DeckPickLogo.png';
 
 function App(props) {
   return (
     <NextUIProvider>
       <div>
-        <img src={image} style={{ position: 'fixed', width: '100%', top: '-10%' }} />
+        <div>
+          <Col>
+            <img src={logo} className='overlayed' style={{ position: 'absolute', width: '5em', left: '1%', top: '2%' }} />
+          </Col>
+          <Col>
+            <Text h4 weight='light' className='overlayed' css={{ position: 'fixed', top: '4.9%', left: '6.5%', color: 'White' }} >DECKPICK</Text>
+          </Col>
+        </div>
+        <img src={image} css={{ position: 'relative', width: '100%', top: '-20%' }} />
         <div className='swipeStack' >
           <SwipePresenter stack_state={props.stack_state} />
         </div>
