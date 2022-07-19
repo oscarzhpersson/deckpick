40f0c4eb8a21e70a57370be57800ac0d09592e16
Oscar Persson <opers@kth.se>
2022-07-19 16:05:06 +0200

    #1 - FEAT: Adds rudimentary stack regeneration when stack runs out
    Could be loaded in the background for seamless transitions, but that's for a later issue

--- deckpick_js/src/components/swipePresenter.jsx
+++ deckpick_js/src/components/swipePresenter.jsx
@@ -16,13 +16,20 @@ function verifyStack (stack)
 
 function SwipePresenter (props)
 {
-    const [stack_promise, set_stack_promise] = React.useState(function initialise(){ return GenerateStack(10, true) });
+    const [stack_promise, set_stack_promise] = React.useState(function initialise(){ return generateStack(10, true) });
     const [stack_data, set_stack_data] = React.useState();
     const [stack_error, set_stack_error] = React.useState();
-    const [stack_index, set_stack_index] = React.useState(10);
 
     const [show_image_modal, set_show_image_modal] = React.useState(false);
     const [current_card, set_current_card] = React.useState();
+
+    var counter = 10;
+
+    function generateStack (quantity, avoid_duplicates)
+    {
+        counter = quantity;
+        return GenerateStack(quantity, avoid_duplicates);
+    }
 
     function showFullImage (card)
     {
@@ -43,7 +50,13 @@ function SwipePresenter (props)
         if(!stack_promise) // If the promise is falsy, return.
             return;
 
-        Promise.all(stack_promise).then(function saveDataACB(d){ if(!cancelled) set_stack_data(d) }).catch(function saveError(e){ if(!cancelled) set_stack_error(e) });
+        Promise.all(stack_promise)
+            .then(function saveDataACB (d) {
+                    if(!cancelled) set_stack_data(d);
+                })
+            .catch(function saveError (e) {
+                    if(!cancelled) set_stack_error(e);
+                });
 
         return onPromiseChangedAgainACB; // onPromiseChangedACB will be called for the new value.
     }
@@ -51,24 +64,21 @@ function SwipePresenter (props)
     React.useEffect(onPromiseChangedACB, [stack_promise]); // When promise changes, run onPromiseChangedACB.
 
     const onSwipe = (direction, id) => {
+
         console.log('You swiped: ' + direction + ' on ' + id);
 
-        var newIndex = stack_index - 1;
-
-        set_stack_index(newIndex);
-
-        console.log(newIndex);
-        // TODO: If card stack is empty, refill.
-
-        if (stack_index < 3)
-        {
-            console.log("PROLOG");
-            set_stack_promise(GenerateStack(15, true));
-        }
+        counter--;
     }
       
     const onCardLeftScreen = (myIdentifier) => {
         console.log(myIdentifier + ' left the screen');
+
+        // TODO: If card stack is empty, refill.
+
+        if (counter <= 0)
+        {
+            set_stack_promise(generateStack(10, true));
+        }
     }
 
     return (
