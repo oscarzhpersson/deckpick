805f32c3a9e3ae71d01430adeb6b99cbad685b01
Oscar Persson <opers@kth.se>
2022-07-20 18:35:30 +0200

    #8 - FIX: Sets absolute coordinates for the logo

--- deckpick_js/src/views/App.js
+++ deckpick_js/src/views/App.js
@@ -14,10 +14,10 @@ function App(props) {
       <div>
         <div>
           <Col justify="flex-end">
-            <img src={logo} className='overlayed' style={{ position: 'absolute', width: '5em', left: '1%', top: '2%' }} />
+            <img src={logo} className='overlayed' style={{ position: 'absolute', width: '5em', left: '1em', top: '1em' }} />
           </Col>
           <Col>
-            <Text h4 weight='light' className='overlayed' css={{ position: 'absolute', top: '9%', left: '4.5%', color: 'White' }} >DECKPICK</Text>
+            <Text h4 weight='light' className='overlayed' css={{ position: 'absolute', top: '2.7em', left: '4.3em', color: 'White' }} >DECKPICK</Text>
           </Col>
         </div>
         <img className='unselectable' src={image} css={{ position: 'relative', width: '100vw', top: '-20%' }} />
